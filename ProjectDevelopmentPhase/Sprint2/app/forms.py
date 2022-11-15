2755e012c4cbe0e20390e53acc2f8b732cce3d25
jagadish <goldjagadish@gmail.com>
2022-11-15 22:39:07 +0530

    added transaction and customize forms

--- ProjectDevelopmentPhase/Sprint2/app/forms.py
+++ ProjectDevelopmentPhase/Sprint2/app/forms.py
@@ -27,3 +27,42 @@ class RegisterForm(FlaskForm):
         existing_email = fetchUserByEmail(email.data)
         if existing_email != []:
             raise ValidationError('This email already exists!')
+
+class Transaction(FlaskForm):
+    # login_id = StringField(render_kw={"placeholder": "Login ID"})
+    transaction = StringField(validators=[
+                           InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Expense Amount"})
+    mode = SelectField('Mode', choices=["Online","Cash"],validators=[
+                           InputRequired()], render_kw={"placeholder": "Category"})
+    category = SelectField('Category', choices=["Housing","Transport","Food","Family","Medical","Debt Payment","Entertainment","Food","Other"],validators=[
+                           InputRequired()])
+    datestamp = DateField('Start Date', format='%Y-%m-%d',validators=[
+                           InputRequired()], render_kw={"placeholder": "Date"})
+    note = StringField(validators=[
+                           InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Note"})
+    submit = SubmitField('Submit Transaction')
+    def validate_transaction(self, transaction):
+        if int(transaction.data) < 0:
+            raise ValidationError('Enter a valid amount')
+
+class Customize(FlaskForm):
+    # login_id = StringField(render_kw={"placeholder": "Login ID"})
+    name = StringField(validators=[InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Name"})
+    budget = StringField('Budget', validators=[InputRequired()], render_kw={"placeholder": "Budget"})
+    total_spent = StringField('Total_Spent', validators=[DataRequired()], render_kw={"placeholder": "Total Spent"})
+    phone = StringField('Phone', validators=[DataRequired()], render_kw={"placeholder": "Phone"})
+    profession = StringField(validators=[InputRequired(), Length(min=1, max=40)], render_kw={"placeholder": "Profession"})
+    alert = BooleanField()
+    submit = SubmitField('Submit Transaction')
+
+    def validate_phone(self, phone):
+        try:
+            p = phonenumbers.parse(phone.data)
+            if not phonenumbers.is_valid_number(p):
+                raise ValueError()
+        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+            raise ValidationError('Invalid phone number')
+        
+    def validate_budget(self, budget):
+        if int(budget.data) < 0:
+            raise ValidationError('Enter a valid budget')
